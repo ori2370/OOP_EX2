75f7bf5ad8b87264def25f51322ed2e865dda745
DanielRivni <drivni123@gmail.com>
2023-01-06 15:01:17 +0200

    upload Ex2_1

---
+++ src/Ex2_1.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.Random;
+
+public class Ex2_1 {
+    public static String[] createTextFiles(int n, int seed, int bound) {
+        String[] fileNames = new String[n];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++) {
+            File file = new File("file_" + (i + 1) + ".txt");
+            fileNames[i] = "file_" + (i + 1) + ".txt";
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+                int randomInt = rand.nextInt(bound);
+                String line = "hara al reshatot";
+                for (int j = 0; j < randomInt; j++) {
+                    bw.write(line);
+                    bw.newLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return fileNames;
+    }
+
+
+    public static int getNumOfLines(String[] fileNames) {
+        int lineCounter = 0;
+        for (int i = 0; i < fileNames.length; i++) {
+            try (BufferedReader br = new BufferedReader(new FileReader(fileNames[i]))) {
+                String line = br.readLine();
+                while (line != null) {
+                    lineCounter++;
+                    line = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return lineCounter;
+    }
+
+    public static int getNumOfLinesThreads(String[] fileNames) {
+        int lineCounter = 0;
+        for (int i = 0; i < fileNames.length; i++) {
+            threadFileHelper thread = new threadFileHelper(fileNames[i],i);
+            thread.start();
+            lineCounter+=thread.getLineCounter();
+
+        }
+
+        return lineCounter;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String[] texts = createTextFiles(3,1,10);
+        long startTime1 = System.nanoTime();
+        int result1 = getNumOfLines(texts);
+        long endTime1 = System.nanoTime();
+        long duration1 = endTime1 - startTime1;
+        long startTime2 = System.nanoTime();
+        int result2 = getNumOfLinesThreads(texts);
+        long endTime2 = System.nanoTime();
+        long duration2 = endTime2 - startTime2;
+        System.out.println("Duration1: " + duration1 + " nanoseconds" + result1);
+        System.out.println("Duration2: " + duration2 + " nanoseconds" + result2);
+
+    }
+}
+
+
---
+++ src/threadFileHelper.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class threadFileHelper extends Thread{
+    private String fileName;
+    private int threadNumber;
+    public int lineCounter=0;
+
+    public threadFileHelper(String fileName,int threadNumber) {
+        this.fileName = fileName;
+        this.threadNumber = threadNumber;
+    }
+
+    public int getLineCounter() {
+        return lineCounter;
+    }
+
+    public void setLineCounter(int lineCounter) {
+        this.lineCounter = lineCounter;
+    }
+
+    @Override
+    public void run (){
+        int Counter = 0;
+        File file = new File(fileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = br.readLine();
+            while (line != null) {
+                Counter++;
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.lineCounter=Counter;
+    }
+    }
+
